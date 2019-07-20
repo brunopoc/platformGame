6efe211b5e5920fa6cac320e32a0caac1447c18b
Bruno <bruno.poc@gmail.com>
2019-07-20 17:18:25 -0300

    [wip]

---
+++ Assets/Scripts/Mechanics/Platform/platform_controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platform_controller : raycast_controller{
+    
+    public Vector3 move;
+
+    public override void Start(){
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Vector3 velocity = move * Time.deltaTime;
+        transform.Translate(velocity);
+    }
+}
