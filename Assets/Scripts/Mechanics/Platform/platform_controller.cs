2082f6f28f0cedd18c4df8c800a2b5016457be2a
Bruno <bruno.poc@gmail.com>
2019-07-22 21:32:09 -0300

    [wip] more tweaks on moving platform

--- Assets/Scripts/Mechanics/Platform/platform_controller.cs
+++ Assets/Scripts/Mechanics/Platform/platform_controller.cs
@@ -7,6 +7,9 @@ public class platform_controller : raycast_controller{
     public Vector3 move;
     public LayerMask passengerMask;
 
+    List<PassengerMovement> passengerMovements;
+    Dictionary<Transform, controller2D> passengerDictionary = new Dictionary<Transform, controller2D>();
+
     public override void Start(){
         base.Start();
     }
@@ -17,13 +20,26 @@ public class platform_controller : raycast_controller{
 
         Vector3 velocity = move * Time.deltaTime;
 
-        MovePassengers(velocity);
+        CalculatePassengerMovement(velocity);
+        MovePassengers (true);
         transform.Translate(velocity);
+        MovePassengers (false);
     }
 
-    void MovePassengers(Vector3 velocity){
+    void MovePassengers(bool beforeMovePlatform){
+        foreach(PassengerMovement passenger in passengerMovements){
+            if(!passengerDictionary.ContainsKey(passenger.transform)){
+                passengerDictionary.Add(passenger.transform, passenger.transform.GetComponent<controller2D>());
+            }
+            if(passenger.moveBeforePlatform == beforeMovePlatform){
+                passengerDictionary[passenger.transform].Move(passenger.velocity, passenger.standingOnPlatform);
+            }
+        }
+    }
 
+    void CalculatePassengerMovement(Vector3 velocity){
         HashSet<Transform> movePassengers = new HashSet<Transform>();
+        passengerMovements = new List<PassengerMovement>();
         float directionX = Mathf.Sign(velocity.x);
         float directionY = Mathf.Sign(velocity.y);
 
@@ -42,8 +58,8 @@ public class platform_controller : raycast_controller{
 						movePassengers.Add(hit.transform);
 						float pushX = (directionY == 1)?velocity.x:0;
 						float pushY = velocity.y - (hit.distance - skinWidth) * directionY;
-
-						hit.transform.Translate(new Vector3(pushX,pushY));
+                        passengerMovements.Add(new PassengerMovement(hit.transform, new Vector3(pushX, pushY), directionY == 1, true));
+						
 					}
 				}
 			}
@@ -59,9 +75,9 @@ public class platform_controller : raycast_controller{
                     if (!movePassengers.Contains(hit.transform)) {
                         movePassengers.Add(hit.transform);
                         float pushX = velocity.x - (hit.distance - skinWidth) * directionX;
-                        float pushY = 0;
+                        float pushY = -skinWidth;
 
-                        hit.transform.Translate(new Vector3(pushX,pushY));
+                        passengerMovements.Add(new PassengerMovement(hit.transform, new Vector3(pushX, pushY), false, true));
                     }
                 }
             }
@@ -82,10 +98,24 @@ public class platform_controller : raycast_controller{
 						float pushX = velocity.x;
 						float pushY = velocity.y;
 
-						hit.transform.Translate(new Vector3(pushX,pushY));
+						passengerMovements.Add(new PassengerMovement(hit.transform, new Vector3(pushX, pushY), true, false));
 					}
 				}
 			}
+        }
+    }
+
+    struct PassengerMovement {
+        public Transform transform;
+        public Vector3 velocity;
+        public bool standingOnPlatform;
+        public bool moveBeforePlatform;
+
+        public PassengerMovement (Transform _transform, Vector3 _velocity, bool _standingOnPlatform, bool _moveBeforePlatform){
+            transform = _transform;
+            velocity = _velocity;
+            standingOnPlatform = _standingOnPlatform;
+            moveBeforePlatform = _moveBeforePlatform;
         }
     }
 }
--- Assets/Scripts/Player/controller2D.cs
+++ Assets/Scripts/Player/controller2D.cs
@@ -14,7 +14,7 @@ public class controller2D : raycast_controller{
         base.Start();
     }
 
-    public void Move(Vector3 velocity){
+    public void Move(Vector3 velocity, bool standingOnPlatform = false){
         UpdateRaycastOrigins();
         collisions.Reset();
         collisions.velocityOld = velocity;
@@ -28,6 +28,10 @@ public class controller2D : raycast_controller{
             VerticalCollisions(ref velocity);
         }
         transform.Translate(velocity);
+
+        if (standingOnPlatform){
+            collisions.below = true;
+        }
     }
 
     void VerticalCollisions(ref Vector3 velocity){
@@ -75,6 +79,10 @@ public class controller2D : raycast_controller{
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask); 
 
             if(hit){
+
+                if(hit.distance == 0){
+                    continue;
+                }
                 
                 float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
 
