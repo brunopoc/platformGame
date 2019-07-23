64dd90fc7d2c39b2e595b80c6cf1d5288204a655
Bruno <bruno.poc@gmail.com>
2019-07-22 21:06:20 -0300

    [wip] simple moving platform

--- Assets/Scripts/Mechanics/Platform/platform_controller.cs
+++ Assets/Scripts/Mechanics/Platform/platform_controller.cs
@@ -48,5 +48,44 @@ public class platform_controller : raycast_controller{
 				}
 			}
 		}
+
+        if(velocity.x != 0){
+            float rayLength  = Mathf.Abs(velocity.x) + skinWidth;
+            for(int i = 0; i < horizontalRayCount; i++){
+                Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+                rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, passengerMask); 
+                if (hit) {
+                    if (!movePassengers.Contains(hit.transform)) {
+                        movePassengers.Add(hit.transform);
+                        float pushX = velocity.x - (hit.distance - skinWidth) * directionX;
+                        float pushY = 0;
+
+                        hit.transform.Translate(new Vector3(pushX,pushY));
+                    }
+                }
+            }
+        }
+
+        if(directionY == -1 || velocity.y == 0 && velocity.x != 0) {
+            float rayLength = skinWidth * 2;
+			
+			for (int i = 0; i < verticalRayCount; i ++) {
+				Vector2 rayOrigin = raycastOrigins.topLeft + Vector2.right * (verticalRaySpacing * i);
+				RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up, rayLength, passengerMask);
+
+                Debug.DrawRay(rayOrigin, Vector2.up * rayLength,Color.red);
+
+				if (hit) {
+					if (!movePassengers.Contains(hit.transform)) {
+						movePassengers.Add(hit.transform);
+						float pushX = velocity.x;
+						float pushY = velocity.y;
+
+						hit.transform.Translate(new Vector3(pushX,pushY));
+					}
+				}
+			}
+        }
     }
 }
